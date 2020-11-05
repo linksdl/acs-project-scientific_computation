be6cf4a7f9776ccbf4467c8235a23194ab5381f2
linksdl <794469928@qq.com>
2020-11-05 09:17:57 +0000

    course works

--- assessment/courework1/CW1_scripts/Jfun_chemical.m
+++ assessment/courework1/CW1_scripts/Jfun_chemical.m
@@ -1,11 +1,25 @@
-function J = Jfun_chemical(a, V, G, k, a0)
+function jf = Jfun_chemical(a, V, G, k, a0)
 
-   J = [
+    % The nonlinear equation system F(U)
+    %
+    % function f = fun_chemical(a, V, G, k, a0)
+    %
+    % Input: 
+    %      a - current solution
+    %      V - constants
+    %      G - constants
+    %      k - constants
+    %     a0 - constants
+    %
+    % Output: 
+    %     jf = Jacobian matrix
+    %
+   jf = [
          (k*V/G)*2*a(1) + 1, 0, 0, 0, 0;
         -1, (k*V/G)*2*a(2) + 1, 0, 0, 0;
         0, -1, (k*V/G)*2*a(3) + 1, 0, 0;
         0, 0, -1, (k*V/G)*2*a(4) + 1, 0;
         0, 0,  0, -1, (k*V/G)*2*a(5) + 1
        ];
-
+   
 end
--- assessment/courework1/CW1_scripts/fun_chemical.m
+++ assessment/courework1/CW1_scripts/fun_chemical.m
@@ -1,14 +1,23 @@
-function F = fun_chemical(a, V, G, k, a0)
+function f = fun_chemical(a, V, G, k, a0)
 
+    % The nonlinear equation system F(U)
     %
+    % function f = fun_chemical(a, V, G, k, a0)
     %
+    % Input: 
+    %      a - current solution
+    %      V - constants
+    %      G - constants
+    %      k - constants
+    %     a0 - constants
     %
+    % Output: 
+    %     f = final function value
     %
-    F = [(k*V/G)*a(1).^2 - a0 + a(1);
+    f = [(k*V/G)*a(1).^2 - a0 + a(1);
          (k*V/G)*a(2).^2 - a(1) + a(2);
          (k*V/G)*a(3).^2 - a(2) + a(3);
          (k*V/G)*a(4).^2 - a(3) + a(4);
          (k*V/G)*a(5).^2 - a(4) + a(5)
-         ];
-     
+         ];    
 end
