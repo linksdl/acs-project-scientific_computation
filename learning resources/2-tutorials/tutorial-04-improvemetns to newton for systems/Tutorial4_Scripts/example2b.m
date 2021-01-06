fd90aa6f31ea88e04af634c8c81226faff175a7e
linksdl <794469928@qq.com>
2021-01-06 21:42:38 +0000

    review

--- learning resources/2-tutorials/tutorial-04-improvemetns to newton for systems/Tutorial4_Scripts/example2b.m
+++ learning resources/2-tutorials/tutorial-04-improvemetns to newton for systems/Tutorial4_Scripts/example2b.m
@@ -8,7 +8,7 @@ x0 = [3;-3.1];    % Newton doesn't converge, everything else does
 %x0 = [-0.2;1.2]; % Newton with line search doesn't converge, everything else does
 
 % NEWTON 
-[x,f] = newtonSys(@exampleFun2, @fdJacobian, x0, 1e-6, 100);
+%[x,f] = newtonSys(@exampleFun2, @fdJacobian, x0, 1e-6, 100);
 
 % NEWTON WITH LINE SEARCH
 %[x,f] = newtonSysL(@exampleFun2, @fdJacobian, x0, 1e-6, 100);
